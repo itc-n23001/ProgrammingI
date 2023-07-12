b98b6c1d80c2a57e76d737812bc3e0cf0e76804a
n23001 <ryugo>
2023-07-12 00:02:16 +0000

    Q2_11_1.py & Q2_11_2.py

---
+++ CHAPTER02/Q2_final.py
@@ -0,0 +1,4 @@
+import random
+
+s = [chr(i) for i in range(97, 123)]
+print(random.randint(97, 123))
---
+++ CHAPTER02/u.py
@@ -0,0 +1,2 @@
+print([chr(i) for i in range(65, 65 + 26)])
+print([chr(ord("A") + i) for i in range(26)])
