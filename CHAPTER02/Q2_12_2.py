777ff410cd5ebd3877f8aed0c9bb63e517aada31
n23001 <ryugo>
2023-07-12 00:14:56 +0000

    2.12の答え

---
+++ CHAPTER02/Q2_12_2.py
@@ -0,0 +1,7 @@
+import random
+
+print(random.randit(0, 10))
+
+# ランダムを使いたい場合はimport randomを使う、上は教科書のコード間違って
+# 下のコードはそれを正しくした場合
+print(random.randit(0, 10))
--- CHAPTER02/u.py
+++
@@ -1,2 +0,0 @@
-print([chr(i) for i in range(65, 65 + 26)])
-print([chr(ord("A") + i) for i in range(26)])
