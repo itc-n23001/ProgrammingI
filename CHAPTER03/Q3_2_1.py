41088b8e2be30b475ab6d4f9b7c3fa7d27e1b8e0
n23001 <ryugo>
2023-07-12 07:08:30 +0000

    3.1~3.4

---
+++ CHAPTER03/Q3_2_1.py
@@ -0,0 +1,3 @@
+a = 10
+a = a / 2 if a > 10 else a + 1
+print(a)
---
+++ CHAPTER03/Q3_3_1.py
@@ -0,0 +1,2 @@
+i = ["tokyo", "osaka", "fukuola", "aichi", "kyoto", "chiba", "saitama", "gunma"]
+print([m for m in i if len(m) >= 6])
