735dd272df9b4ede87b1d05a92a6812d7fd53071
n23001 <ryugo>
2023-08-17 06:50:19 +0000

    7.1~7.2answer

---
+++ CHAPTER07/Q7_1_3.py
@@ -0,0 +1,7 @@
+# 次のプログラムを実行した時に発生する例外の型はどれでしょうか
+int("3.5")
+# 1.indexError
+# 2.KeyError
+# 3.ZeroDivisionError
+# 4.ValueError
+# answer 4
---
+++ CHAPTER07/Q7_1_4.py
@@ -0,0 +1,4 @@
+my_lsit = []
+my_list.insert(3)
+
+# TypeErrorが出力されます
